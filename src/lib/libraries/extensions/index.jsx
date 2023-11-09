573c7f8bc1dd3f4fea7270a5b401b61e7d3b34af
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-09 10:08:36 -0600

    Update index.jsx

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -331,7 +331,7 @@ const menuItems = [
     },
     {
         name: 'Stupidity',
-        extensionId: 'https://raw.githubusercontent.com/SY-Gato/Scratch-Plugins/main/Stupidity/Stupidity.js',
+        extensionId: 'blob:https://raw.githubusercontent.com/SY-Gato/Scratch-Plugins/main/Stupidity/Stupidity.js',
         tags: ['categoryexpansion'],
         iconURL: 'https://extensions.penguinmod.com/images/ObviousAlexC/PenPlus.svg',
         description: "Some random utilities i made. There really are some stupid utils in here.",
